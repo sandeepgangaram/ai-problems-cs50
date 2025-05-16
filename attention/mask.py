b7d9dd6f8f7231f4a0d7d8fac9be0d8a379db3ca
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2025-05-16 09:01:12 +0530

    get shade color for attention score

--- attention/mask.py
+++ attention/mask.py
@@ -54,15 +54,13 @@ def get_mask_token_index(mask_token_id, inputs):
     return None
 
 
-
 def get_color_for_attention_score(attention_score):
     """
     Return a tuple of three integers representing a shade of gray for the
     given `attention_score`. Each value should be in the range [0, 255].
     """
-    # TODO: Implement this function
-    raise NotImplementedError
-
+    val = int(attention_score*255)
+    return (val, val, val)
 
 
 def visualize_attentions(tokens, attentions):
